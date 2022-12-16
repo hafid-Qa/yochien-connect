6a0d64b36805f9d6fa73840e58be9e240f707e08
joycehwchan <joycehwchan@gmail.com>
2022-12-16 12:53:03 +0900

    added mui data-grid

--- app/javascript/front_end/components/Children.jsx
+++ app/javascript/front_end/components/Children.jsx
@@ -1,14 +1,42 @@
 import React from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+
 import Child from "./Child";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
+  { field: "age", headerName: "Age", type: "number", width: 90 },
+];
+
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
 
 const Children = () => {
   return (
-    <div class="container mt-5">
+    <div className="container mt-5">
       <h1>Children#index</h1>
-      <p>*Will switch to Material UI data table later.</p>
-      <Child />
-      <Child />
-      <Child />
+      <p>Fake data for now.</p>
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          components={{ Toolbar: GridToolbar }}
+        />
+      </div>
     </div>
   );
 };
