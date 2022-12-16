81351e67750207280feada24041290d8dac260a3
Hafid Qarchi <haqarchi@gmail.com>
2022-12-16 11:11:07 +0900

    association and validation for user and trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :child
   belongs_to :user
+  validates :pick_up_time, presence: true
+  validates :drop_off_time, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :children_as_parent, class_name: "Child", foreign_key: :parent_id, dependent: :destroy
   has_many :children_as_teacher, class_name: "Child", foreign_key: :teacher_id
+  has_many :trips
   validates :email, presence: true
   validates :full_name, presence: true
   # validates :type, presence: true
