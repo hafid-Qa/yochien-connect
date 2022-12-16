dbda5a2b53137d77cb3906a944b593ba63733e0b
joycehwchan <joycehwchan@gmail.com>
2022-12-16 11:18:02 +0900

    setup view skeleton

---
+++ app/javascript/front_end/components/Children.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Child from "./Child";
+
+const Children = () => {
+  return (
+    <div class="container mt-5">
+      <h1>Children#index</h1>
+      <p>*Will switch to Material UI data table later.</p>
+      <Child />
+      <Child />
+      <Child />
+    </div>
+  );
+};
+
+export default Children;
