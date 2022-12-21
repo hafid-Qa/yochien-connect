586a38a7bdf2d2166636c6c0a9c46f8f526fb859
Hafid Qarchi <haqarchi@gmail.com>
2022-12-21 17:16:26 +0900

    update status from db

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,5 +1,6 @@
 class Child < ApplicationRecord
   attribute :age, type: :date
+  attribute :status, type: :string
 
   belongs_to :parent, class_name: "User"
   has_many :child_in_trips
@@ -7,9 +8,12 @@ class Child < ApplicationRecord
   validates :full_name, presence: true
   validates :full_address, presence: true
   validates :full_name, presence: true
-  
 
   def age
     DateTime.current.to_date.year - birthday.year
+  end
+
+  def status
+    child_in_trips.where(trip_date: DateTime.current.to_date)[0].status
   end
 end
--- db/migrate/20221219004120_add_status_to_child_in_trips.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToChildInTrips < ActiveRecord::Migration[7.0]
-  def change
-    add_column :child_in_trips, :Status, :integer
-  end
-end
