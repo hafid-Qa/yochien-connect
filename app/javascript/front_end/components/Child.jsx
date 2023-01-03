f5004661859d0c678d63ccaaf05be53364485916
joycehwchan <joycehwchan@gmail.com>
2023-01-03 15:48:53 +0900

    handle dropdown change

--- app/javascript/front_end/components/Child.jsx
+++ app/javascript/front_end/components/Child.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { statuses } from "../mockData/data";
 
-const Child = ({ name, age, status, address, handleChange }) => {
+const Child = ({ child, handleChange }) => {
   const profilImgUrl =
     "https://imgs.search.brave.com/93vIQeqFrqeopRkmyUBgIEnccTvF-Qsl5opsKSbzw-c/rs:fit:900:900:1/g:ce/aHR0cHM6Ly95dDMu/Z2dwaHQuY29tL2Ev/QUFUWEFKeXhpWGI0/TVhBMGVMZl92a0ZV/NHRuUU15VVdGV1dq/QUZzb2lNVzFiUT1z/OTAwLWMtay1jMHhm/ZmZmZmZmZi1uby1y/ai1tbw";
 
@@ -12,25 +12,24 @@ const Child = ({ name, age, status, address, handleChange }) => {
           <img src={profilImgUrl} alt="" className="child-img" />
           <div className="child-detail">
             <div className="child-name">
-              <h5 className="m-0">{name}</h5>
+              <h5 className="m-0">{child.full_name}</h5>
             </div>
             <div className="child-data">
-              <p className="m-0">Age: {age}</p>
-              <p className="m-0">Address: {address}</p>
+              <p className="m-0">Age: {child.age}</p>
+              <p className="m-0">Address: {child.full_address}</p>
             </div>
           </div>
         </div>
-        {/* <button className="child-status">{status}</button> */}
         <select
           id="child-status"
           name="child-status"
-          defaultValue={status}
-          onChange={(e) => handleChange(e)}
+          defaultValue={child.status}
+          onChange={(e) => handleChange(e, child)}
         >
-          {statuses.map((stat, i) => {
+          {statuses.map((status, i) => {
             return (
-              <option value={stat} key={i}>
-                {stat}
+              <option value={status} key={i}>
+                {status}
               </option>
             );
           })}
--- app/javascript/front_end/components/Children.jsx
+++ app/javascript/front_end/components/Children.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
-import { statuses } from "../mockData/data";
+import { filters } from "../mockData/data";
 
 import Child from "./Child";
 
@@ -25,8 +25,26 @@ const Children = (props) => {
     setActive(index);
   };
 
-  const handleChange = (e) => {
-    console.log(e.target.value);
+  const handleChange = (e, child) => {
+    const new_status = e.target.value;
+    console.log(new_status);
+
+    // TODO: update database with API
+    console.log("updating database...");
+    console.log("updated");
+
+    // TODO: update virtual DOM (update value of dropdown)
+    // setChildren((prevChildren) => {
+    //   prevChildren.map((prev_child) => {
+    //     return child.id === prev_child.id
+    //       ? {
+    //           ...prev_child,
+    //           status: new_status,
+    //         }
+    //       : prev_child;
+    //   });
+    // });
+    // console.log(children);
   };
 
   return (
@@ -35,7 +53,7 @@ const Children = (props) => {
       <h3 className="mt-1 mb-3">{moment().format("h:mm a")}</h3>
 
       <div className="filter-container">
-        {statuses.map((status, index) => {
+        {filters.map((filter, index) => {
           return (
             <span
               className={`${
@@ -46,7 +64,7 @@ const Children = (props) => {
                 handleClick(e, index);
               }}
             >
-              {status}
+              {filter}
             </span>
           );
         })}
@@ -55,14 +73,7 @@ const Children = (props) => {
       <div className="row gap-2">
         {children.map((child) => {
           return (
-            <Child
-              key={child.id}
-              name={child.full_name}
-              age={child.age}
-              status={child.status}
-              address={child.full_address}
-              handleChange={handleChange}
-            />
+            <Child key={child.id} child={child} handleChange={handleChange} />
           );
         })}
       </div>
