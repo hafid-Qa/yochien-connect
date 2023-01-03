57f04d774e5a210e2c861a1fff5cd6080ed27f08
Hafid Qarchi <haqarchi@gmail.com>
2023-01-03 12:05:30 +0900

    redifined the child model policy by removing the trip

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,21 +1,7 @@
 class ChildrenController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index]
 
-  def create
-  end
-
   def index
-    query_trip = if Time.new.localtime.hour >= 14
-                   ChildInTrip.where(trip_date: DateTime.current.to_date, trip_id: Trip.first.id)
-                 else
-                   ChildInTrip.where(trip_date: DateTime.current.to_date, trip_id: Trip.second.id)
-                 end
-    @children = query_trip.map { |trip| policy_scope(Child.find(trip.child_id)) }
-  end
-
-  def update
-  end
-
-  def cancel_transport
+    @children = policy_scope(Child)
   end
 end
