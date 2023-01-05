7841398c15478c4540698b6e16f5ff1d49292187
Hafid Qarchi <haqarchi@gmail.com>
2023-01-05 17:16:18 +0900

    data controller added to chatroom show page

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,7 +1,8 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
   connect() {
+    console.log("subscribe");
   }
 }
