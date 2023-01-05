cb5db218e4f4e2abdc1c4872058f10665b8e5083
Hafid Qarchi <haqarchi@gmail.com>
2023-01-05 17:28:43 +0900

    imported create consumer from actioncable

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,8 +1,19 @@
 import { Controller } from "@hotwired/stimulus";
-
+import { createConsumer } from "@rails/actioncable";
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
+  static values = { chatroomId: Number };
+  static targets = ["messages"];
   connect() {
-    console.log("subscribe");
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      {
+        received: (data) => {
+          // insert it into the messages div
+          this.messagesTarget.insertAdjacentHTML("beforeend", data);
+          this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight);
+        },
+      }
+    );
   }
 }
