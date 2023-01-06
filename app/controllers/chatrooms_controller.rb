a4e92ba751aa10c1eb2b8d726122fca71b2e1303
Hafid Qarchi <haqarchi@gmail.com>
2023-01-07 00:16:26 +0900

    dislaying all users

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,12 +2,15 @@ class ChatroomsController < ApplicationController
   def index
     @message = Message.new
     @chatrooms = policy_scope(Chatroom)
+    @users = User.all_except(current_user)
+    @chatroom = Chatroom.new
   end
 
   def show
     @chatrooms = Chatroom.all
     @message = Message.new
     @chatroom = Chatroom.find(params[:id])
+    @users = User.all_except(current_user)
     authorize @chatroom
     render "index"
   end
