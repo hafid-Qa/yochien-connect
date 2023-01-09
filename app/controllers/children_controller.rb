7ce6dbd6ab45cd3b688ed90efc774ad0513e572c
Hafid Qarchi <haqarchi@gmail.com>
2023-01-09 13:54:11 +0900

    authenticate user bofore actions on children controller

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,5 +1,5 @@
 class ChildrenController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index]
+  before_action :authenticate_user!, only: %i[index]
 
   def index
     @children = policy_scope(Child)
