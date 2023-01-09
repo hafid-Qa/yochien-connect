cf400e8c518cee7b66c2cb7befbc10788b8d380d
Hafid Qarchi <haqarchi@gmail.com>
2023-01-09 14:07:46 +0900

    typo in method name

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :chatroom
   belongs_to :user
-  before_create :confirm_participants
+  before_create :confirm_participant
 
-  def confirm_participants
+  def confirm_participant
     return unless chatroom.is_private
 
     is_participant = Participant.where(user_id: user.id, chatroom_id: chatroom.id).first
