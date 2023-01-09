944623d9d96300f566f6c0c0d73c99a192da1155
Hafid Qarchi <haqarchi@gmail.com>
2023-01-09 13:37:45 +0900

    refactor code in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_users, only: :show
 
   def show
-    @user = User.find(params[:id])
-    @users = User.all_parents
-    @admins = User.all_admins
-    authorize @user
-
-    @chatrooms = Chatroom.public_chatrooms
+    @chatrooms = policy_scope(Chatroom)
     @chatroom = Chatroom.new
     @chatroom_name = "private_#{@user.id}"
     @single_chatroom = set_single_chatroom
@@ -20,8 +16,17 @@ class UsersController < ApplicationController
 
   private
 
+  def set_users
+    @user = User.find(params[:id])
+    @users = User.all_parents
+    @admins = User.all_admins
+    authorize @user
+  end
+
   def set_single_chatroom
-    Chatroom.where(name: @chatroom_name).first || Chatroom.create_private_chatroom([@user, current_user, @admins].flatten,
-                                                                                   @chatroom_name)
+    set_chatroom = Chatroom.where(name: @chatroom_name).first
+    create_private_chatroom = Chatroom.create_private_chatroom([@user, current_user, @admins].flatten,
+                                                               @chatroom_name)
+    set_chatroom || create_private_chatroom
   end
 end
